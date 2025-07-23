bf4d46ab34ad7276f8bb2179daf0e60a69b92e79
Phil Huang <phil.huang@microsoft.com>
2025-07-23 22:51:50 +0800

    ✨ Add new script for document conversion using Markitdown and create an initial markdown file for document intelligence standards.
    Signed-off-by: Phil Huang <phil.huang@microsoft.com>

---
+++ azure-ai-document-intelligence/markitdown/conversion-by-markitdown.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source .env
+ENDPOINT_DOCUMENT_INTELLIGENCE="https://aif-pichuang-jpe.services.ai.azure.com/"
+
+markitdown 衛生福利部醫療領域資通系統資安防護基準核定本.pdf \
+    --output test-markitdown.md
