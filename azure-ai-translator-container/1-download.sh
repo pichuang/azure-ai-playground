4d9e382e4d7d958e6f168d36973a8002cc888352
Phil Huang <phil.huang@microsoft.com>
2025-08-21 23:48:24 +0800

    🛠️ Update error message in download script to include command for creating .env file
    Signed-off-by: Phil Huang <phil.huang@microsoft.com>

--- azure-ai-translator-container/1-download.sh
+++ azure-ai-translator-container/1-download.sh
@@ -4,6 +4,8 @@
 if [ ! -f .env ]; then
   echo ".env file is missing"
   echo "Please create a .env file based on the .env.example file"
+  echo "Command:"
+  echo "cp .env.example .env"
   exit 1
 fi
 
