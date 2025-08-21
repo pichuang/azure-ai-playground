d522b587445130e4f2d82035823fd27a72deee79
Phil Huang <phil.huang@microsoft.com>
2025-08-22 00:21:30 +0800

    🛠️ Refactor script to include container image download and verification steps for improved setup clarity.
    Signed-off-by: Phil Huang <phil.huang@microsoft.com>

--- azure-ai-translator-container/0-preapre.sh
+++ azure-ai-translator-container/0-preapre.sh
@@ -6,3 +6,15 @@ mkdir -p ./azure-ai-translator/license
 sudo chown -R 65532:65532 ./azure-ai-translator
 sudo chmod -R o+w ./azure-ai-translator
 
+
+# Download Container Image
+docker pull mcr.microsoft.com/azure-cognitive-services/translator/text-translation:latest
+
+# Verified Container Image
+docker images --format "table {{.Repository}}\t{{.Tag}}\t{{.ID}}"
+
+# Save Container image
+docker save -o azure-translator-text-translation.tar mcr.microsoft.com/azure-cognitive-services/translator/text-translation:latest
+
+# Check tarball
+ls -lh azure-translator-text-translation.tar
