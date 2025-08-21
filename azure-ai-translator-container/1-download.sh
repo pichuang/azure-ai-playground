1be11b88fbb79fc854f522b541ff231875105904
Phil Huang <phil.huang@microsoft.com>
2025-08-21 23:46:31 +0800

    🛠️ Add .env file existence check in download script
    Signed-off-by: Phil Huang <phil.huang@microsoft.com>

--- azure-ai-translator-container/1-download.sh
+++ azure-ai-translator-container/1-download.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# Check .env is existing
+if [ ! -f .env ]; then
+  echo ".env file is missing"
+  echo "Please create a .env file based on the .env.example file"
+  exit 1
+fi
+
 docker compose -f download-models-docker-compose.yaml up
