522d972fb67cceadc179a3882b41cf54171a02ab
Phil Huang <phil.huang@microsoft.com>
2025-08-22 00:38:32 +0800

    🛠️ Update tar command to include file permissions for better archive integrity.
    Signed-off-by: Phil Huang <phil.huang@microsoft.com>

--- azure-ai-translator-container/2-tar.sh
+++ azure-ai-translator-container/2-tar.sh
@@ -4,4 +4,4 @@ cd ..
 
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 
-tar -czvf "azure-ai-translator-container-archive-${TIMESTAMP}.tar.gz" azure-ai-translator-container
+tar -czvpf "azure-ai-translator-container-archive-${TIMESTAMP}.tar.gz" azure-ai-translator-container
