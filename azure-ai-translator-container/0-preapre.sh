ece4e04e7217941264f7c2b4e4bb0ca075d381a2
Phil Huang <phil.huang@microsoft.com>
2025-08-21 23:43:59 +0800

    🛠️ Remove unnecessary ACL settings from the setup script for Azure AI Translator container.
    Signed-off-by: Phil Huang <phil.huang@microsoft.com>

--- azure-ai-translator-container/0-preapre.sh
+++ azure-ai-translator-container/0-preapre.sh
@@ -5,5 +5,4 @@ mkdir -p ./azure-ai-translator/logs
 mkdir -p ./azure-ai-translator/license
 sudo chown -R 65532:65532 ./azure-ai-translator
 sudo chmod -R o+w ./azure-ai-translator
-sudo setfacl -R -m d:o::rw,o::rw ./azure-ai-translator
 
